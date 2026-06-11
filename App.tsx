6ee0a069e5561edcfabaeebd689fd09f41a514c5
Mauricio Rojas <mrojasb2000@gmail.com>
2026-06-11 17:30:09 -0400

    feat: add basic counter

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(10);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textL}>My Counter App!</Text>
+      <Text style={styles.textXL}>{count}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#574D84',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textL: {
+    fontSize: 20,
+    fontWeight: 500,
+    color: '#fff'
+  },
+  textXL: {
+    fontSize: 40,
+    fontWeight: 500,
+    color: '#fff'
+  },
 });
+
+
