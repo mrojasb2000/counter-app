420add8fd8707075475e8aba0b1f1f4ad5d56802
Mauricio Rojas <mrojasb2000@gmail.com>
2026-08-23 15:34:41 -0400

    ref: presseable button

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Floating from './components/buttons/Floating';
 
 export default function App() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
   const intervalRef = useRef<number | null>(null);
 
   const handlePressIn = () => {
     if (intervalRef.current) clearInterval(intervalRef.current);
     intervalRef.current = setInterval(() => {
       setCount(prev => prev + 1);
-    }, 50);
+    }, 150);
   };
 
   const handlePressOut = () => {
@@ -21,25 +21,50 @@ export default function App() {
     }
   };
 
+  const handlePlusOne = () => {
+    setCount(prev => prev + 1);
+  }
+
+  const handleMinusOne = () => {
+    if (count == 0) return;
+    setCount(prev => prev - 1);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={[styles.text, styles.textL]}>My Counter App!</Text>
       <Text style={[styles.text, styles.textXL]}>{count}</Text>
-      <Floating
+      {/* <Floating
         label="Add 1"
         position='right'
         OnPress={() => setCount(count + 1)}
         OnPressIn={handlePressIn}
         OnPressOut={handlePressOut}
         labelStyle={styles.textL}
-      />
-      <Floating
+      /> */}
+      {/* <Floating
         label=" Reset "
         position='left'
         OnPress={() => setCount(0)}
         labelStyle={styles.textL}
         buttonStyle={{ backgroundColor: '#67d9b7' }}
-      />
+      /> */}
+      <Pressable style={({ pressed }) => [
+        styles.floatingButtonMinusOne,
+        {
+          backgroundColor: pressed ? '#aaaaab' : '#65558F', // Dark gray on press, purple normally
+        },
+      ]} onPress={handleMinusOne} onLongPress={() => setCount(0)}>
+        <Text style={{ color: 'white'}}>-1</Text>
+      </Pressable>
+      <Pressable style={({ pressed }) => [
+        styles.floatingButtonPlusOne,
+        {
+          backgroundColor: pressed ? '#aaaaab' : '#65558F', // Dark gray on press, purple normally
+        },
+      ]} onPress={handlePlusOne} onLongPress={() => setCount(0)}>
+        <Text style={{ color: 'white'}}>+1</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -48,13 +73,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#574D84',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text:{
     fontWeight: 500,
-    color: '#fff',
+    color: '#65558F',
   },
   textL: {
     fontSize: 20,
@@ -62,6 +87,32 @@ const styles = StyleSheet.create({
   textXL: {
     fontSize: 40,
   },
+  floatingButtonMinusOne: {
+    position: 'absolute',
+    bottom: 80,
+    left: 80,
+    backgroundColor: '#65558F',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 3,
+    shadowRadius: 4,
+  },
+  floatingButtonPlusOne: {
+    position: 'absolute',
+    bottom: 80,
+    right: 80,
+    backgroundColor: '#65558F',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 3,
+    shadowRadius: 4,
+  }
 });
 
 
